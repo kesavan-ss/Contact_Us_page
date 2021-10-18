7bd887fd7e44a46da32c47a8cc50aa7ccfb3884e
kesavan-ss <83690915+kesavan-ss@users.noreply.github.com>
2021-10-18 21:37:32 +0530

    Delete store.js

--- store.js
+++
@@ -1,9 +0,0 @@
-function check(){
-    var email=document.forms["cform"]["mail"];
-    var name=document.forms["cform"]["name"];
-    var subject=document.forms["cform"]["subject"];
-    var message=document.forms["cform"]["message"];
-    var query=document.forms["cform"]["query"];
-
-
-}
